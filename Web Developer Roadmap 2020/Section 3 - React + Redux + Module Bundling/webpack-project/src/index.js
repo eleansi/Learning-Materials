b5f088b19679bbf23d3e9534b8ba412365a2462f
Ilian Stefanov <ilian.stefanov@strypes.eu>
2019-12-04 11:28:30 +0200

    renamed folder

--- Web Developer Roadmap 2020/Section 3 - React + Redux + Module Bundling/webpack-project/src/index.js
+++ Web Developer Roadmap 2020/Section 3 - React + Redux + Module Bundling/webpack-project/src/index.js
@@ -1,11 +1,5 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-const name = "Smutario"
-const displayName = (name) => {
-    console.log(name);
-};
-
-displayName(name);
 
 
 // ReactDOM.render(<h1>smut</h1>, document.getElementById('app'));
